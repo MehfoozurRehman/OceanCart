d3612574d13d9ea0dfd07cb589738887beead0d8
Mehfooz-ur-Rehman <mehfoozijaz786@gmail.com>
2021-06-10 12:28:41 +0500

    commit

--- src/components/BtnComponent.js
+++ src/components/BtnComponent.js
@@ -1,8 +1,8 @@
 import React from "react";
 
-export default function BtnComponent({ placeholder, btnStyle, icon }) {
+export default function BtnComponent({ placeholder, btnStyle, icon, type }) {
   return (
-    <button className="form__btn" style={btnStyle}>
+    <button className="form__btn" style={btnStyle} type={type}>
       {placeholder}
       {icon}
     </button>
--- src/components/InputBox.js
+++ src/components/InputBox.js
@@ -1,14 +1,12 @@
-import React from 'react'
+import React from "react";
 
-
-export default function InputBox({type,placeholder,inputStyle}) {
-    
-    return (
-        <input
-              type={type}
-              className="login__form__input"
-              placeholder={placeholder}
-              style={inputStyle}
-            />
-    )
+export default function InputBox({ type, placeholder, inputStyle }) {
+  return (
+    <input
+      type={type}
+      className="login__form__input"
+      placeholder={placeholder}
+      style={inputStyle}
+    />
+  );
 }
